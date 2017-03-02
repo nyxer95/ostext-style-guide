29276e11eb7b6c5531c19b36374e4085bddbeaa9
Derek Ford <inconceivablevizzini@gmail.com>
2017-03-02 03:11:50 -0500

    Add webpack build, compiling es6.

--- frontend/Gulpfile.js
+++ frontend/Gulpfile.js
@@ -2,6 +2,7 @@ require("babel-polyfill"); // Fixes some undefined is not a function bug when re
                            // Object.assign
 
 var gulp = require('gulp');
+var webpack = require('webpack-stream');
 var riot = require('gulp-riot');
 var concat = require('gulp-concat');
 var es = require('event-stream');
@@ -11,10 +12,14 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
+  dist: process.cwd() + '/dist/',
   tags: 'src/tags/*.tag',
   index: 'src/index.html',
-  js: 'src/js/*.js',
-  sass: 'src/scss/*.scss'
+  js: 'src/js/**/*.js',
+  jsdeps: 'src/jsdeps/*.js',
+  sass: 'src/scss/*.scss',
+  js_index: process.cwd() + '/src/js/app.js',
+  js_bundle_fname: 'style-guide.js'
 };
 
 var sassOptions = {
@@ -25,9 +30,9 @@ var sassOptions = {
 
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass','riot'], function() {
+gulp.task('serve', ['sass','build'], function() {
   browserSync.init({
-    server: "./src"
+    server: "./dist"
   });
 
   gulp.watch(paths.sass, ['sass']);
@@ -42,34 +47,42 @@ gulp.task('sass',function() {
       .pipe(sass(sassOptions).on('error', sass.logError))
       .pipe(sourcemaps.write())
       .pipe(autoprefixer())
-      .pipe(gulp.dest("src/css"))
+      .pipe(gulp.dest("dist/css"))
       .pipe(browserSync.stream());
 });
 
-gulp.task('riot', function() {
-  return gulp.src(paths.tags)
-      .pipe(riot())
-      .pipe(concat('tags.js'))
-      .pipe(gulp.dest('src/js'))
-});
-
-// create a task that ensures the `riotjs` task is complete before
+// create a task that ensures the `build` task is complete before
 // reloading browsers
-gulp.task('js-watch', ['riot'], function(done) {
+gulp.task('js-watch', ['build'], function(done) {
   browserSync.reload();
   done();
 });
 
 gulp.task('build', function() {
   return es.merge(
-    gulp.src(paths.tags)
-      .pipe(riot())
-      .pipe(concat('tags.js'))
-      .pipe(gulp.dest('dist/js')),
+    //gulp.src(paths.tags)
+    //  .pipe(riot())
+    //  .pipe(concat('tags.js'))
+    //  .pipe(gulp.dest('dist/js')),
     gulp.src(paths.index)
-      .pipe(gulp.dest('dist')),
+      .pipe(gulp.dest(paths.dist)),
+    //gulp.src(paths.jsdeps)
+    //  .pipe(gulp.dest('dist/js')),
     gulp.src(paths.js)
-      .pipe(gulp.dest('dist/js')),
+    //  .pipe(babel({presets:['es2015']}))
+    //  .pipe(concat('style-guide.js'))
+      .pipe(webpack({ entry: paths.js_index,
+                      output: {
+                        path: paths.dist,
+                        filename: paths.js_bundle_fname
+                      },
+                      module: {
+                        loaders: [{
+                          exclude: /node_modules/,
+                          loader: 'babel-loader'
+                        }]
+                      }}))
+      .pipe(gulp.dest(paths.dist + 'js')),
     gulp.src('src/data/*.json')
       .pipe(gulp.dest('dist/data')),
     gulp.src(paths.sass)
